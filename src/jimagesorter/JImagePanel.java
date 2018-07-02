c0c28f278878e83070da79e0c54c45f7e699667e
James Curran <j.curran@auckland.ac.nz>
2018-07-03 08:36:11 +1200

    Image rescaling now working

--- src/jimagesorter/JImagePanel.java
+++ src/jimagesorter/JImagePanel.java
@@ -7,6 +7,7 @@ package jimagesorter;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
 
@@ -36,39 +37,35 @@ public class JImagePanel extends JPanel {
             int panelWidth = getWidth();
             int panelHeight = getHeight();
             
-//            if(imageWidth > panelWidth || imageHeight > panelHeight){
-//                double aspectRatio = (double)imageWidth / (double)imageHeight;
-//                int newWidth, newHeight;
-//                
-//                // rescale the height then change the width to pre
-//                if(imageWidth > panelWidth){
-//                    double widthScaleFactor = (double)panelWidth / (double)imageWidth;
-//                    newWidth = (int)(widthScaleFactor * imageWidth);
-//                    newHeight = (int)(widthScaleFactor * imageWidth / aspectRatio);
-//                }else{
-//                    double heightScaleFactor = (double)panelHeight / (double)imageHeight;
-//                    newHeight = (int)(heightScaleFactor * imageHeight);
-//                    newWidth = (int)(heightScaleFactor * imageHeight * aspectRatio);
-//                }
-//                
-//                //BufferedImage scaledImage = (BufferedImage)img.getScaledInstance(newWidth, newHeight, BufferedImage.SCALE_DEFAULT);
-//                BufferedImage scaledImage = new BufferedImage(newWidth, newHeight, img.getType());
-//                int x = (panelWidth - newWidth) / 2;
-//                int y = (panelHeight - newHeight) / 2;
-//                
-//                //Graphics2D g2d = (Graphics2D) g.create();
-//                Graphics2D g2d = scaledImage.createGraphics();
-//                //g2d.drawImage(scaledImage, x, y, this);
-//                g2d.drawImage(img, 0, 0, newWidth, newHeight, null);
-//                g2d.dispose();
-//            }else{
+            if(imageWidth > panelWidth || imageHeight > panelHeight){
+                int newWidth, newHeight;
+                
+                // rescale the height then change the width to pre
+                if(imageWidth > imageHeight){
+                    double widthScaleFactor = (double)panelWidth / (double)imageWidth;
+                    newWidth = panelWidth;
+                    newHeight = (int)(widthScaleFactor * imageHeight);
+                }else{
+                    double heightScaleFactor = (double)panelHeight / (double)imageHeight;
+                    newHeight = panelHeight;
+                    newWidth = (int)(heightScaleFactor * imageWidth);
+                }
+                
+                Image scaledImage = img.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+                int x = (panelWidth - newWidth) / 2;
+                int y = (panelHeight - newHeight) / 2;
+                
+                Graphics2D g2d = (Graphics2D) g.create();
+                g2d.drawImage(scaledImage, x, y, this);
+                g2d.dispose();
+            }else{
                 int x = (getWidth() - img.getWidth()) / 2;
                 int y = (getHeight() - img.getHeight()) / 2;
                 
                 Graphics2D g2d = (Graphics2D) g.create();
                 g2d.drawImage(img, x, y, this);
                 g2d.dispose();
-         //   }
+            }
         }
     }
 }
